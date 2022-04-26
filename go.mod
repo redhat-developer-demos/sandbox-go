51f2ef8eb69c1c00ac4a44947c8bcfbfde1e26f3
Don Schenck <don.schenck@gmail.com>
2022-04-26 17:01:23 -0400

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/donschenck/sandbox-go
+
+go 1.12
+
+require (
+	github.com/gin-gonic/contrib v0.0.0-20201101042839-6a891bf89f19 // indirect
+	github.com/gin-gonic/gin v1.7.7 // indirect
+)
---
+++ hello.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/hello", func(c *gin.Context) {
+		c.String(200, "Hello, World!")
+	})
+
+	api := r.Group("/api")
+
+	api.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Use(static.Serve("/", static.LocalFile("./views", true)))
+
+	r.Run()
+}
