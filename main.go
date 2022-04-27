b80067104504c48b2c89dcad508ad093dd72726d
Don Schenck <don.schenck@gmail.com>
2022-04-27 11:16:48 -0400

    added link to main sandbox page

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ type activity struct {
 
 // list of activities.
 var activities = []activity{
+	{Title: "More activities)", URL: "https://developers.redhat.com/developer-sandbox/activities"},
 	{Title: "Event-Driven Architecture using Apache Kafka (with Red Hat OpenShift Streams for Apache Kafka)", URL: "https://developers.redhat.com/node/226211"},
 	{Title: "Deploy a Java Application on Kubernetes in minutes", URL: "https://developers.redhat.com/node/226211"},
 	{Title: "Learn Kubernetes", URL: "https://developers.redhat.com/node/226211"},
